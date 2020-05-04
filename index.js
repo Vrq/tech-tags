c82d04770b99c438bbb93b00f8bbdd451bf9bbb8
Janusz Tomasik <januszveiro@gmail.com>
2020-05-05 00:00:32 +0200

    Add more tags

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 module.exports = {
     techTags: [
-      "Java", "JavaScript", "React", "Vue", "C#", "Python", "Angular", "Docker", "Node", "SQL", "Spring", "TypeScript", "Windows"
+      "Java", "JavaScript", "React", "Vue", "C#", "Python", "Angular", "Docker", "NodeJS", "SQL", "Spring", "TypeScript", "Windows",
+      "CMS", "ExpressJS", "MongoDB", "Ruby", "Rails", "PostgreSQL", "MS-SQL", "MySQL", "CSS", "VR", "DevOps", "Kubernetes", "VMware", "SAP",
+      "Flutter", "Dart", "Git", "Scrum", "JIRA", "Kafka", "Swift", "Zookpeeper", "Jenkins", "Kotlin", "Terraform", "Ansible", "Grafana",
+      "OpenShift", "Ubuntu", "Linux", "RHEL", "GCP", "AWS", "Azure", "C++", "Eclipse", "Avaloq", "Embedded", ".NET", "Django", "Nginx", "PHP", "VR",
+      "Kanban", "Hibernate", "Oracle", "Cloud Foundry", "REST", "JSON", "XML", "API", "Salesforce", "Play Framework", "TDD", "Sketch", "Indesign",
+      "Marvel", "Zeplin", "JavaFX", "Matlab", "Gulp", "SASS", "LESS", ".NET MVC", "RabbitMQ", "ElasticSearch", "Objective-C", "Android", "iOS", "Firebase",
+      "Fastlane", "TSQL", "Redgate", "Redux", "GraphQL", "Mesos", "Sonar", "Cassandra", "Bash", "Unix", "Prometheus", "CI/CD", "Redis","RSpec", "WAF", "IAM", 
+      "Airlock", "Security", "NextJS", "NuxtJS", "Puppet", "ELK", "MariaDB", "Go", "numpy", "pandas", "TensorFlow", "opencv", "pytest", "Bootstrap", "Electron",
+      "BitTalk", "UML", "microservices", "SAML", "Celery", "Mobile", "Scala", "Zend", "Ionic", "Cordova","JEE", "PWA", "Webpack", "PL SQL", "JBOSS", "JUnit", 
+      "Spring Boot", "Swing", "Visual Studio", "WordPress", "GNU", "XP", "Groovy", "ActiveMQ", "Quarkus", "Maven", "JPA", "Bamboo", "E2E", "Cucumber", "Machine Learning",
+      "Golang", "Laravel", "Figma", "Akka", "Spark", "CQRS", "Sentry", "TYPO3", "Drupal", "jQuery"
     ]
 }
