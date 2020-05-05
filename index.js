0c7e2aeaeaeee2e049509c5bc6e0cffce477a304
Janusz Tomasik <januszveiro@gmail.com>
2020-05-05 11:46:21 +0200

    all current technologies

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ module.exports = {
       "Java", "JavaScript", "React", "Vue", "C#", "Python", "Angular", "Docker", "NodeJS", "SQL", "Spring", "TypeScript", "Windows",
       "CMS", "ExpressJS", "MongoDB", "Ruby", "Rails", "PostgreSQL", "MS-SQL", "MySQL", "CSS", "VR", "DevOps", "Kubernetes", "VMware", "SAP",
       "Flutter", "Dart", "Git", "Scrum", "JIRA", "Kafka", "Swift", "Zookpeeper", "Jenkins", "Kotlin", "Terraform", "Ansible", "Grafana",
-      "OpenShift", "Ubuntu", "Linux", "RHEL", "GCP", "AWS", "Azure", "C++", "Eclipse", "Avaloq", "Embedded", ".NET", "Django", "Nginx", "PHP", "VR",
+      "OpenShift", "Ubuntu", "Linux", "RHEL", "GCP", "AWS", "Azure", "C++", "Eclipse", "Avaloq", "Embedded", "ASP .NET", "Django", "Nginx", "PHP", "VR",
       "Kanban", "Hibernate", "Oracle", "Cloud Foundry", "REST", "JSON", "XML", "API", "Salesforce", "Play Framework", "TDD", "Sketch", "Indesign",
       "Marvel", "Zeplin", "JavaFX", "Matlab", "Gulp", "SASS", "LESS", ".NET MVC", "RabbitMQ", "ElasticSearch", "Objective-C", "Android", "iOS", "Firebase",
       "Fastlane", "TSQL", "Redgate", "Redux", "GraphQL", "Mesos", "Sonar", "Cassandra", "Bash", "Unix", "Prometheus", "CI/CD", "Redis","RSpec", "WAF", "IAM", 
       "Airlock", "Security", "NextJS", "NuxtJS", "Puppet", "ELK", "MariaDB", "Go", "numpy", "pandas", "TensorFlow", "opencv", "pytest", "Bootstrap", "Electron",
       "BitTalk", "UML", "microservices", "SAML", "Celery", "Mobile", "Scala", "Zend", "Ionic", "Cordova","JEE", "PWA", "Webpack", "PL SQL", "JBOSS", "JUnit", 
       "Spring Boot", "Swing", "Visual Studio", "WordPress", "GNU", "XP", "Groovy", "ActiveMQ", "Quarkus", "Maven", "JPA", "Bamboo", "E2E", "Cucumber", "Machine Learning",
-      "Golang", "Laravel", "Figma", "Akka", "Spark", "CQRS", "Sentry", "TYPO3", "Drupal", "jQuery"
+      "Golang", "Laravel", "Figma", "Akka", "Spark", "CQRS", "Sentry", "TYPO3", "Drupal", "jQuery", "Symfony", "Pimcore", "Laravel", "React Native",
+      "Clojue", "WPF", "DDD", "Swing","Heroku", "EC2", "SQLite", "Vagrant", "PaaS", "IaaS", "ESB", "SharePoint", "System Engineer", "Yarn", "SOAP", "GIS",
+      "3D", "WebGL", "JWT", "Dapper", "ES6", "OSS", "Redis", "Selenium", "Nexus", "Bamboo", "Cypress", "Gradle", "XPath", "Beanstalk", "Active Directory",
+      "RDF", "BSD", "LDAP", "Cisco", "Asterisk", "Akka", "Teamcity", "ODI", "IBM", "FPGA", "gRPC", "Tomcat", "JBoss", "Websphere", "Office365", "Haskell",
+      "Karma", "Jasmin", "Jest", "Ember", "Backbone", "Vagrant", "Web3", "Elixir", "Erlang", "Spark", "ETL", "BPMN", "AEM", "Leaflet", "Flink",
+      "SailsJS", "HSM", "Qt", "Laravel", "Zend", "Flask", "Celery", "AJAX", "JSP", "Terraform", "Solr", "UiPath", "VBA", "RPA", "Magento", "Composer",
+      "Vaadin", "Tomcat", "Xamarin", "SCSS", "RxJs", "Perl", "Rust", "Phoenix", "Meteor", "Laravel", "Polymer", "D3", "Material UI", "Apollo", "HTTP" 
     ]
+
+    // Golang -> Go
+    // DDD -> Domain Driven
+    // TDD -> Test Driven
+    // Office 365 -> 365
 }
