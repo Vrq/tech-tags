14a0f88935ce2df4afe2662856e283d934df0853
Janusz Tomasik <januszveiro@gmail.com>
2020-05-05 11:49:31 +0200

    Sort and remove duplicates

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 module.exports = {
-    techTags: [
-      "Java", "JavaScript", "React", "Vue", "C#", "Python", "Angular", "Docker", "NodeJS", "SQL", "Spring", "TypeScript", "Windows",
-      "CMS", "ExpressJS", "MongoDB", "Ruby", "Rails", "PostgreSQL", "MS-SQL", "MySQL", "CSS", "VR", "DevOps", "Kubernetes", "VMware", "SAP",
-      "Flutter", "Dart", "Git", "Scrum", "JIRA", "Kafka", "Swift", "Zookpeeper", "Jenkins", "Kotlin", "Terraform", "Ansible", "Grafana",
-      "OpenShift", "Ubuntu", "Linux", "RHEL", "GCP", "AWS", "Azure", "C++", "Eclipse", "Avaloq", "Embedded", "ASP .NET", "Django", "Nginx", "PHP", "VR",
-      "Kanban", "Hibernate", "Oracle", "Cloud Foundry", "REST", "JSON", "XML", "API", "Salesforce", "Play Framework", "TDD", "Sketch", "Indesign",
-      "Marvel", "Zeplin", "JavaFX", "Matlab", "Gulp", "SASS", "LESS", ".NET MVC", "RabbitMQ", "ElasticSearch", "Objective-C", "Android", "iOS", "Firebase",
-      "Fastlane", "TSQL", "Redgate", "Redux", "GraphQL", "Mesos", "Sonar", "Cassandra", "Bash", "Unix", "Prometheus", "CI/CD", "Redis","RSpec", "WAF", "IAM", 
-      "Airlock", "Security", "NextJS", "NuxtJS", "Puppet", "ELK", "MariaDB", "Go", "numpy", "pandas", "TensorFlow", "opencv", "pytest", "Bootstrap", "Electron",
-      "BitTalk", "UML", "microservices", "SAML", "Celery", "Mobile", "Scala", "Zend", "Ionic", "Cordova","JEE", "PWA", "Webpack", "PL SQL", "JBOSS", "JUnit", 
-      "Spring Boot", "Swing", "Visual Studio", "WordPress", "GNU", "XP", "Groovy", "ActiveMQ", "Quarkus", "Maven", "JPA", "Bamboo", "E2E", "Cucumber", "Machine Learning",
-      "Golang", "Laravel", "Figma", "Akka", "Spark", "CQRS", "Sentry", "TYPO3", "Drupal", "jQuery", "Symfony", "Pimcore", "Laravel", "React Native",
-      "Clojue", "WPF", "DDD", "Swing","Heroku", "EC2", "SQLite", "Vagrant", "PaaS", "IaaS", "ESB", "SharePoint", "System Engineer", "Yarn", "SOAP", "GIS",
-      "3D", "WebGL", "JWT", "Dapper", "ES6", "OSS", "Redis", "Selenium", "Nexus", "Bamboo", "Cypress", "Gradle", "XPath", "Beanstalk", "Active Directory",
-      "RDF", "BSD", "LDAP", "Cisco", "Asterisk", "Akka", "Teamcity", "ODI", "IBM", "FPGA", "gRPC", "Tomcat", "JBoss", "Websphere", "Office365", "Haskell",
-      "Karma", "Jasmin", "Jest", "Ember", "Backbone", "Vagrant", "Web3", "Elixir", "Erlang", "Spark", "ETL", "BPMN", "AEM", "Leaflet", "Flink",
-      "SailsJS", "HSM", "Qt", "Laravel", "Zend", "Flask", "Celery", "AJAX", "JSP", "Terraform", "Solr", "UiPath", "VBA", "RPA", "Magento", "Composer",
-      "Vaadin", "Tomcat", "Xamarin", "SCSS", "RxJs", "Perl", "Rust", "Phoenix", "Meteor", "Laravel", "Polymer", "D3", "Material UI", "Apollo", "HTTP" 
-    ]
+  techTags: [".NET MVC", "3D", "AEM", "AJAX", "API", "ASP .NET", "AWS", "Active Directory", "ActiveMQ", "Airlock", "Akka", "Android",
+    "Angular", "Ansible", "Apollo", "Asterisk", "Avaloq", "Azure", "BPMN", "BSD", "Backbone", "Bamboo", "Bamboo", "Bash", "Beanstalk", "BitTalk",
+    "Bootstrap", "C#", "C++", "CI/CD", "CMS", "CQRS", "CSS", "Cassandra", "Celery", "Celery", "Cisco", "Clojue", "Cloud Foundry", "Composer", "Cordova",
+    "Cucumber", "Cypress", "D3", "DDD", "Dapper", "Dart", "DevOps", "Django", "Docker", "Drupal", "E2E", "EC2", "ELK", "ES6", "ESB", "ETL", "Eclipse",
+    "ElasticSearch", "Electron", "Elixir", "Embedded", "Ember", "Erlang", "ExpressJS", "FPGA", "Fastlane", "Figma", "Firebase", "Flask", "Flink",
+    "Flutter", "GCP", "GIS", "GNU", "Git", "Go", "Golang", "Gradle", "Grafana", "GraphQL", "Groovy", "Gulp", "HSM", "HTTP", "Haskell", "Heroku",
+    "Hibernate", "IAM", "IBM", "IaaS", "Indesign", "Ionic", "JBoss", "JEE", "JIRA", "JPA", "JSON", "JSP", "JUnit", "JWT", "Jasmin", "Java",
+    "JavaFX", "JavaScript", "Jenkins", "Jest", "Kafka", "Kanban", "Karma", "Kotlin", "Kubernetes", "LDAP", "LESS", "Laravel",
+    "Leaflet", "Linux", "MS-SQL", "Machine Learning", "Magento", "MariaDB", "Marvel", "Material UI", "Matlab", "Maven", "Mesos", "Meteor"
+    , "Mobile", "MongoDB", "MySQL", "NextJS", "Nexus", "Nginx", "NodeJS", "NuxtJS", "ODI", "OSS", "Objective-C", "Office365", "OpenShift",
+    "Oracle", "PHP", "PL SQL", "PWA", "PaaS", "Perl", "Phoenix", "Pimcore", "Play Framework", "Polymer", "PostgreSQL", "Prometheus", "Puppet",
+    "Python", "Qt", "Quarkus", "RDF", "REST", "RHEL", "RPA", "RSpec", "RabbitMQ", "Rails", "React", "React Native", "Redgate", "Redis",
+    "Redux", "Ruby", "Rust", "RxJs", "SAML", "SAP", "SASS", "SCSS", "SOAP", "SQL", "SQLite", "SailsJS", "Salesforce", "Scala", "Scrum", "Security",
+    "Selenium", "Sentry", "SharePoint", "Sketch", "Solr", "Sonar", "Spark", "Spring", "Spring Boot", "Swift", "Swing", "Symfony",
+    "System Engineer", "TDD", "TSQL", "TYPO3", "Teamcity", "TensorFlow", "Terraform", "Tomcat", "TypeScript", "UML", "Ubuntu",
+    "UiPath", "Unix", "VBA", "VMware", "VR", "VR", "Vaadin", "Vagrant", "Visual Studio", "Vue", "WAF", "WPF", "Web3", "WebGL", "Webpack",
+    "Websphere", "Windows", "WordPress", "XML", "XP", "XPath", "Xamarin", "Yarn", "Zend", "Zend", "Zeplin", "Zookpeeper", "gRPC", "iOS", "jQuery",
+    "microservices", "numpy", "opencv", "pandas", "pytest"]
 
-    // Golang -> Go
-    // DDD -> Domain Driven
-    // TDD -> Test Driven
-    // Office 365 -> 365
+  // Golang -> Go
+  // DDD -> Domain Driven
+  // TDD -> Test Driven
+  // Office 365 -> 365
 }
