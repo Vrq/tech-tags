b87a070aef83af351d914bb322ad4bb43d8c3cac
Janusz Tomasik <januszveiro@gmail.com>
2020-05-14 14:23:38 +0200

    Add new tech

--- index.js
+++ index.js
@@ -80,13 +80,13 @@ const techTags = [".NET MVC", "3D", "AEM", "AJAX", "API", "ASP.NET", "AWS", "Act
   "Hibernate", "IAM", "IBM", "IaaS", "Indesign", "Ionic", "JBoss", "JIRA", "JPA", "JSON", "JSP", "JUnit", "JWT", "Jasmine", "Java", "Java EE",
   "JavaFX", "JavaScript", "Jenkins", "Jest", "Kafka", "Kanban", "Karma", "Kotlin", "Kubernetes", "LDAP", "LESS", "Laravel",
   "Leaflet", "Linux", "MS-SQL", "Machine Learning", "Magento", "MariaDB", "Marvel", "Material UI", "Matlab", "Maven", "Mesos", "Meteor",
-  "Mobile", "MongoDB", "MySQL", "NextJS", "Nexus", "Nginx", "NodeJS", "NoSQL", "NuxtJS", "ODI", "OSS", "Objective-C", "Office365", "OpenShift",
+  "Mobile", "MongoDB", "MySQL", "NextJS", "Nexus", "Nginx", "NodeJS", "NoSQL", "NuxtJS", "ODI", "OSS", "Objective-C", "Office365", "OpenGL", "OpenShift",
   "Oracle", "PHP", "PL SQL", "PWA", "PaaS", "Perl", "Phoenix", "Pimcore", "Play Framework", "Polymer", "PostgreSQL", "Prometheus", "Puppet",
   "Python", "Qt", "Quarkus", "RDF", "REST", "RHEL", "RPA", "RSpec", "RabbitMQ", "Rails", "React", "React Native", "Redgate", "Redis",
   "Redux", "Remote Work", "Ruby", "Rust", "RxJs", "SAML", "SAP", "SASS", "SCSS", "SOAP", "SQL", "SQLite", "SailsJS", "Salesforce", "Scala", "Scrum", "Security",
-  "Selenium", "Sentry", "SharePoint", "Sketch", "Solr", "Sonar", "Spark", "Spring", "Spring Boot", "Swagger", "Swift", "Swing", "Symfony",
-  "System Engineer", "TDD", "TSQL", "TYPO3", "Teamcity", "TensorFlow", "Terraform", "Tomcat", "TypeScript", "UML", "Ubuntu",
-  "UiPath", "Unix", "UX UI Design", "VBA", "VMware", "VR", "Vaadin", "Vagrant", "Visual Studio", "Vue", "WAF", "WPF", "Web3", "WebGL", "Webpack",
+  "Selenium", "Sentry", "SharePoint", "Sketch", "Solr", "Sonar", "Spark", "Splunk", "Spring", "Spring Boot", "Swagger", "Swift", "Swing", "Symfony",
+  "System Engineer", "TDD", "TSQL", "TYPO3", "Teamcity", "TensorFlow", "Terraform", "Three.js", "Tomcat", "TypeScript", "UML", "Ubuntu",
+  "UiPath", "Unix", "Unity", "Unreal", "UX UI Design", "VBA", "VMware", "VR", "Vaadin", "Vagrant", "Visual Studio", "Vue", "WAF", "WPF", "Web3", "WebGL", "Webpack",
   "Websphere", "Windows", "WordPress", "XML", "XP", "XPath", "Xamarin", "Yarn", "Zend", "Zeplin", "Zookpeeper", "gRPC", "iOS", "jQuery",
   "microservices", "numpy", "opencv", "pandas", "pytest"]
 
